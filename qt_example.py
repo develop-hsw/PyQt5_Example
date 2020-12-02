67ff4212e678f767d250f6a87b4faee0f0653cf7
develop-hsw <poohpama@naver.com>
2020-12-02 16:50:17 +0900

    [2020-12-02] QThread 적용

--- qt_example.py
+++ qt_example.py
@@ -1,24 +1,50 @@
 from PyQt5 import uic
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QTimer, QTime
+from PyQt5.QtCore import QTimer, QTime, QObject, pyqtSignal, pyqtSlot, QThread
+# qthread -> global / queue / list 로 대기를 쌓는 방법.
+# python deck / qt 자체 thread에서 제공하는 data pipeline.
 
+import time
 import sys
 import os
-import cv2
+from collections import deque
 
 path = './sample_imgs/'
 form_class = uic.loadUiType("qt_designer.ui")[0]
 
 running_state = False
-# 설정문제같다.
+
+class Thread1(QThread):
+    def run(self):
+        count = 0
+        while count < 5:
+            time.sleep(1)
+            print("count", count)
+            count += 1
+            if(count == 5):
+                count = 0
+
+
 class Ui_Dialog(QDialog, form_class) :
+    sig_number = pyqtSignal()
+
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
-
         self.timer = QTimer(self)  # 타이머 객체 생성
         self.timer.timeout.connect(self.fibonacci)  # 슬롯 timeout() 객체 호출
+
+    number = 0
+    @pyqtSlot()
+    def numbering(self):
+        x = Thread1(self)
+        x.start()
+    """
+    number = 0
+    number += 1
+    ui.h_1_textlabel.setText(str(self.number))"""
+
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
@@ -31,28 +57,18 @@ class Ui_Dialog(QDialog, form_class) :
         self.h_3_button.setText(_translate("Dialog", "이미지 바꾸기"))
 
 
-    number = 0
-    def numbering(self):
-        timeVar = QTimer()
-        timeVar.setInterval(1000)
-        timeVar.start()
-        self.h_1_textlabel.setText(str(self.number))
-        self.number += 1
-
-
     a, b = 0, 1
-    flag = False
     def fibonacci(self):
-        for i in range(0,10000000):
-            print(i)
+        for i in range(1000000):
+            if(i % 10 == 0):
+                print(i)
         if running_state == True :
             self.timer.start(1000 * 1)  # 1초마다 타이머 실행, 시작
             self.a, self.b = self.b, self.a + self.b
             self.h_2_textlabel.setText(str(self.a))
         else:
             self.timer.stop()
-
-
+    # with fibonacci
     def start(self):
         global running_state
         if running_state == False:
@@ -65,7 +81,6 @@ class Ui_Dialog(QDialog, form_class) :
 
     idx = 0
     filelist = os.listdir(path)
-    print(filelist)
     def image_load(self):
         pixmap = QtGui.QPixmap(path)
         pixmap.load(path+"images_{0}.jpg".format(self.idx))
@@ -80,11 +95,11 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
+
     ui.setupUi(Dialog)
 
     ui.h_1_button.clicked.connect(ui.numbering)
     ui.h_2_button.clicked.connect(ui.start)
-
     ui.h_3_button.clicked.connect(ui.image_load)
 
     Dialog.show()
